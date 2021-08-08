ec1add9cbd6e4cbecbf19dd972b68b8603866592
rudrapawar <78693479+rudrapawar@users.noreply.github.com>
2021-08-07 21:53:52 -0400

    CNAME Changes

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -1,9 +1,6 @@
 import dayjs from 'dayjs';
 
-/* Keys match keys returned by the github api. Fields without keys are
- * mostly jokes. To see everything returned by the github api, run:
- curl https://api.github.com/repos/mldangelo/personal-site
- */
+
 const data = [
   {
     label: 'Stars this repository has on github',
